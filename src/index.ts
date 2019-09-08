249834849fa3fb5ccf745f20bc00b8d9ceea83f3
Martin Koparanov <m.n.koparanov@gmail.com>
2019-09-08 18:11:51 +0300

    Implement debounceAnimationFrame

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export const test = () => 'mest';
+const debounceAnimationFrame = <T>(fn: Function) => {
+  let timeout: number;
+  const debouncedFn = (...args: any) => {
+    cancelAnimationFrame(timeout);
+    return new Promise<T>(resolve => {
+      timeout = requestAnimationFrame(() => {
+        const result: T = fn(...args);
+        resolve(result);
+      });
+    })
+  
+  };
+  return debouncedFn;
+};
+
+export default debounceAnimationFrame;
